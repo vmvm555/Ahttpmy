f45b020d9df3f87aec0744bb152edfabb36bc3c9
join <join@join-PC>
2015-07-13 15:39:47 +0800

    1

--- src/com/loopj/android/http/AsyncHttpClient.java
+++ src/com/loopj/android/http/AsyncHttpClient.java
@@ -122,7 +122,7 @@ import android.util.Log;
  * @see com.loopj.android.http.RequestParams
  */
 
-// a
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 public class AsyncHttpClient {
 
 	public static final String LOG_TAG = "AsyncHttpClient";
