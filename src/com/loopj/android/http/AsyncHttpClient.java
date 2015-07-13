9b9946961beff2d5a56c0b7eb185b3a0fc68913e
join <join@join-PC>
2015-07-13 14:50:22 +0800

    没找到更新button

--- src/com/loopj/android/http/AsyncHttpClient.java
+++ src/com/loopj/android/http/AsyncHttpClient.java
@@ -122,7 +122,7 @@ import android.util.Log;
  * @see com.loopj.android.http.RequestParams
  */
 
-// asynchttp ��һ��
+// asyn
 public class AsyncHttpClient {
 
 	public static final String LOG_TAG = "AsyncHttpClient";
